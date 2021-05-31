46288cb6e688562ee3f9d3607c706b18daa31ab7
Guglielmo Del Sarto <guglielmo@MacBook-Air-di-Guglielmo.local>
2021-05-31 12:47:59 +0200

    Version 1.0.0

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.guglielmoDelSarto.economics.experiments"},{"l":"com.guglielmoDelSarto.economics.optimalPlan"},{"l":"com.guglielmoDelSarto.economics.production"},{"l":"com.guglielmoDelSarto.economics.utilityFunctions"},{"l":"com.guglielmoDelSarto.general.discounting"},{"l":"com.guglielmoDelSarto.general.time"}]
